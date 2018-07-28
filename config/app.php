728ea80f1a256c97dd5f4fdcfc97780944e9d2ed
leomeee <wanglei3@dangegongyu.com>
2018-07-28 20:38:53 +0800

    excel 加入别名

--- config/app.php
+++ config/app.php
@@ -209,8 +209,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => \Tymon\JWTAuth\Facades\JWTAuth::class,
-        'JWTFactory'=>\Tymon\JWTAuth\Facades\JWTFactory::class
-
+        'JWTFactory' => \Tymon\JWTAuth\Facades\JWTFactory::class,
+        'Excel' => \Maatwebsite\Excel\Facades\Excel::class
     ],
 
 ];
